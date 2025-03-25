8a3615f66d31129354793ef2cc2fad903644ac6b
ErickBernat <erickduartebr@gmail.com>
2025-03-25 18:01:54 -0300

    Fix Responsividade Concluida

--- script/script.js
+++ script/script.js
@@ -1,27 +1,65 @@
 let listaLinks = document.getElementById('listaLinks');
+let listaLinksCortada = document.getElementById('listaLinksCortada');
 let botaoEntrar = document.getElementById('botaoEntrar');
-var controla = false;
+let botaoMenuHamburguer =  document.querySelector('.Menu-Hamburguer-Botao');
+let MenuHamburguer =  document.querySelector('#Menu-Hamburguer');
+let listaLinksHamburguer = document.querySelector('.inputs')
+let imagemEpic = document.querySelector('.imageNav');
+
+
+var controlaHambuger = false;
 var entrouNaDiv = false;
 
-window.addEventListener("DOMContentLoaded", adicionaLinkLista);
+window.addEventListener("DOMContentLoaded", adicionaLinkListaCortada);
+
+if(botaoMenuHamburguer.style.display == 'none'){
+
+}else{
+
+    botaoMenuHamburguer.addEventListener('click',()=>{
+        if(controlaHambuger == false){
+         MenuHamburguer.style.display = 'block';
+         MenuHamburguer.style.height="100vh"
+         
+     
+         imagemEpic.style.display ='none';
+         controlaHambuger = true;
+        }else{
+          MenuHamburguer.style.display = 'none'
+          imagemEpic.style.display ='block';
+         controlaHambuger=false
+        }
+     })
+
+}
+
+
+
+
 
 let links = [
     { nome: 'Visão geral',subNomes:['Unreal Engine','Destaques','Licenciamento','Perguntas Frequentes'], class: 'rotaciona', id: 1},
     { nome: 'Usos',subNomes:['Mudança para a Unreal Engine ','Jogos AAA ','Jogos independentes','Jogos para celular','Unreal Editor para Fortnite','Filmes e TV ','Transmissões e eventos ao vivo '], class: 'rotaciona', id: 2 },
-    { nome: 'Novidades' },
-    { nome: 'Fóruns' },
-    { nome: 'Documentação' },
-    { nome: 'Aprendizados',subNomes:['Biblioteca de Aprendizado','Introduçao','Repositório de Trecho de código'], class: 'rotaciona', id: 3 },
-    { nome: 'Conectar',subNomes:['Eventos','MegaGrants','MeetUps','Parceiros de Serviço','Estudantes e Escolas','Treinamento Profissional','Parceiros Academicos e de Treinamento'], class: 'rotaciona', id: 4 },
-    { nome: 'Mais',subNomes:['MarketPlace'], class: 'rotaciona', id: 5 }
+    { nome: 'Novidades',id: 3},
+    { nome: 'Fóruns',id: 4 },
+    { nome: 'Documentação',id: 5 },
+    { nome: 'Aprendizados',subNomes:['Biblioteca de Aprendizado','Introduçao','Repositório de Trecho de código'], class: 'rotaciona', id: 6 },
+    { nome: 'Conectar',subNomes:['Eventos','MegaGrants','MeetUps','Parceiros de Serviço','Estudantes e Escolas','Treinamento Profissional','Parceiros Academicos e de Treinamento'], class: 'rotaciona', id: 7 },
+    { nome: 'Mais',subNomes:['MarketPlace'], class: 'rotaciona', id: 8 }
 ];
 
-function adicionaLinkLista() {
+
+
+
+
+
+
+function adicionaLinkListaCortada() {
     let i = 0;
 
-    if(window.innerWidth <= 1200){
-       links = links[0]
-    };
+
+ 
+
     links.forEach(item => {
         
         if (item.class == 'rotaciona') {
@@ -35,74 +73,64 @@ function adicionaLinkLista() {
                     `
                 })
             }
-            listaLinks.innerHTML += `
+            
+
+            listaLinksCortada.innerHTML += `
             <div class='conteudoMenu'>
                 <div class='conteudo_rotaciona'>
                     <li href="" id="link-${item.id}" ">${item.nome}</li>
                     <img  class="seta ${item.class}" src="./assets/images/seta.png" alt="Seta" >
                 </div>
                 <div id="div-${item.id}" class="divSobrepoe" style="display: none;">
-                     ${linksHtml}
+                    ${linksHtml}
                 </div>
             </div>
             `;
+            listaLinksHamburguer.innerHTML+=`
+            <a href="" id="link-${item.id}" ">${item.nome}</a>
+        `;
             
         } else {
-            listaLinks.innerHTML += `
+            listaLinksHamburguer.innerHTML+=`
                 <a href="" id="link-${item.id}" ">${item.nome}</a>
-            `;
+            `
         }
     });
 
-    let subListas=document.querySelectorAll('.divSobrepoe');
-
-
-
-
-   
-
-    
-
-
     links.forEach(item => {
         if (item.class == 'rotaciona') {
-            let tagA = document.getElementById(`link-${item.id}`);
-            let div = document.getElementById(`div-${item.id}`);
-            let timeout;  
-    
-       
-            tagA.addEventListener('mouseenter', () => {
-                div.style.display = 'block';
-                div.style.opacity = 1;
-                div.style.zIndex = 50;
-    
-                
+            let tagAs = document.getElementById(`link-${item.id}`);
+            let diva = document.getElementById(`div-${item.id}`);
+            let timeout;
+   
+            tagAs.addEventListener('mouseenter', () => {
+                diva.style.display = 'block';
+                diva.style.opacity = 1;
+                diva.style.zIndex = 50;
                 clearTimeout(timeout);
             });
-    
-           
-            tagA.addEventListener('mouseleave', () => {
+   
+            tagAs.addEventListener('mouseleave', () => {
                 timeout = setTimeout(() => {
-                    div.style.opacity = 0;
+                    diva.style.opacity = 0;
                     setTimeout(() => {
                         div.style.display = 'none';
                     }, 100); 
                 }, 100); 
             });
-    
-          
-            div.addEventListener('mouseenter', () => {
+   
+            diva.addEventListener('mouseenter', () => {
                 div.style.opacity = 1; 
                 clearTimeout(timeout);
             });
-    
-         
-            div.addEventListener('mouseleave', () => {
-                div.style.opacity = 0; 
+   
+            diva.addEventListener('mouseleave', () => {
+                diva.style.opacity = 0; 
                 setTimeout(() => {
-                    div.style.display = 'none';
+                    diva.style.display = 'none';
                 }, 100);
             });
         }
     });
+
 }
